4b3a660ded266528c61dcdc050f764304a6939a9
Vincent Son <107471539+szw0407@users.noreply.github.com>
2025-03-30 16:44:32 +0800

    Upd

--- app/src/main/java/com/example/sleephh25a/MainActivity.java
+++ app/src/main/java/com/example/sleephh25a/MainActivity.java
@@ -27,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    static long timeout = 1803;  // half an hour
-    static long timeleft= 1803; // left
+    static long timeout = 1801;  // half an hour
+    static long timeleft= 1801; // left
     static long time0  = 0;   // start to sleep
     static long timeP = 0;   // 上次修改睡眠时间长短的时间，3s后才能改动
     static int pass = 0;    // counter of sensor callback
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                         //if (xyz>30) 改为TYPE_GYROSCOPE
                         timeleft = timeout;
                     }
-                    //if (xyz>30) // here
+
 
                 }
                 // 一个失败的尝试，算了。还是回到上面if(xyz>30)条件吧
@@ -95,25 +95,39 @@ public class MainActivity extends AppCompatActivity {
         Sensor s2 = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         sm.registerListener(el,s1,SensorManager.SENSOR_DELAY_UI);
         sm.registerListener(el,s2,SensorManager.SENSOR_DELAY_UI);
-
+        final boolean[] status = {false};  // has not yet achieved the timeout
         // 消息处理函数
         Handler flush = new Handler(new Handler.Callback() {
             @Override
             public boolean handleMessage(@NonNull Message msg) {
                 if (msg.what != 5107)
                     return true;
-
-                if (timeleft<=3) {
+                if (status[0]) return true;
+                if (timeleft<=1) {
                     MediaPlayer m = MediaPlayer.create(MainActivity.this, R.raw.alife);
                     //mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
                     m.setLooping(true);
                     m.start();
-                    timeleft=300; // retry later
+                    Toast.makeText(MainActivity.this, "time out", Toast.LENGTH_LONG).show();
+                    status[0] = true;
+                    // 任意触碰屏幕停止播放
+                    // 先显示这行字在倒计时的地方
+                    ((TextView)findViewById(R.id.id_helloworld)).setText("time out, touch screen to restart");
+                    findViewById(R.id.main).setOnClickListener(v -> {
+                        m.stop();
+                        m.release();
+                        timeleft = timeout;
+                        status[0] = false;
+                        ((TextView)findViewById(R.id.id_helloworld)).setText("time left " + timeleft/60 + "m:" + timeleft%60+"s");
+                        findViewById(R.id.main).setOnClickListener(null);
+                    });
+
                 }
-                else
-                    timeleft = timeleft-3;
+                else{
+                    timeleft = timeleft-1;
 
                 ((TextView)findViewById(R.id.id_helloworld)).setText("time left " + timeleft/60 + "m:" + timeleft%60+"s");
+                }
                 return true;//false;
             }
         });
@@ -126,9 +140,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // 每3秒，持续1小时
         Timer timer1m = new Timer();
-        timer1m.schedule(new Every1m(), 3*1000, 3*1000);
+        timer1m.schedule(new Every1m(), 1000, 1000);
 
     }
 
